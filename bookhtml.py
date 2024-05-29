3fa569b50663e8a5de1e6ef12dc82a8ac34401de
0x4D0x00 <94094679+0x4D0x00@users.noreply.github.com>
2024-05-29 22:33:01 +0800

    Update bookhtml.py

--- bookhtml.py
+++ bookhtml.py
@@ -12,14 +12,20 @@ def rsc(url):
     
     try:
         response = requests.get(url, timeout = 5)
+        content = response.text
         statusCode = response.status_code
         #print(statusCode)
         if statusCode == 200:
-            with open('书签.html', 'a') as htmlFile:
-                dtA = f'	<DT><A HREF="{url}" ADD_DATE={Datetime}">{url}</A>\n'
-                htmlFile.writelines(dtA)
-        else:
-            pass
+            if "IIS7" in content:
+                with open('IIS7Ports.txt', 'a') as file:
+                file.write(url + '\n')
+            elif "NGINX" in content:
+                with open('nginxPorts.txt', 'a') as file:
+                    file.write(url + '\n')
+            elif "限制" not in content and "无法正常工作" not in content:
+                with open('书签.html', 'a') as htmlFile:
+                    dtA = f'	<DT><A HREF="{url}" ADD_DATE={Datetime}">{url}</A>\n'
+                    htmlFile.write(dtA)
     except:
         pass
 
@@ -29,15 +35,15 @@ if __name__ == "__main__":
         st = f'<!DOCTYPE NETSCAPE-Bookmark-file-1>\n<!-- This is an automatically generated file.\n     It will be read and overwritten.\n     DO NOT EDIT! -->\n<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">\n<TITLE>Bookmarks</TITLE>\n<H1>Bookmarks</H1>\n<DL><p>\n    <DT><H3 ADD_DATE="2716597092" LAST_MODIFIED="0" PERSONAL_TOOLBAR_FOLDER="true">BOOK</H3>\n    <DL><p>\n    </DL><p>\n'
         htmlFile.writelines(st)
     #获取url
-    with open('scanPorts.txt', 'r') as scanFile:
-        urlList = [url.strip() for url in scanFile]
+    with open('urlPorts.txt', 'r') as scanFile:
+        urlsList = list(set([url.strip() for url in scanFile if url.strip()]))
     #获取可访问页面
     print(" | 主线任务进度 |")
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        for result in tqdm(executor.map(rsc, urlList), total=len(urlList)):
+        for result in tqdm(executor.map(rsc, urlsList), total=len(urlsList)):
             pass
     #书签完成
     with open('书签.html', 'a') as htmlFile:
         dlp = '</DL><p>\n'
-        htmlFile.writelines(dlp)
+        htmlFile.write(dlp)
     print('任务执行完成')
