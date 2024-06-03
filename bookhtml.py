27a8575557001732b1d7eee41dac44c0410a3f50
0x4D0x00 <94094679+0x4D0x00@users.noreply.github.com>
2024-06-03 10:28:38 +0800

    Update bookhtml.py

--- bookhtml.py
+++ bookhtml.py
@@ -40,8 +40,11 @@ if __name__ == "__main__":
         st = f'<!DOCTYPE NETSCAPE-Bookmark-file-1>\n<!-- This is an automatically generated file.\n     It will be read and overwritten.\n     DO NOT EDIT! -->\n<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">\n<TITLE>Bookmarks</TITLE>\n<H1>Bookmarks</H1>\n<DL><p>\n    <DT><H3 ADD_DATE="2716597092" LAST_MODIFIED="0" PERSONAL_TOOLBAR_FOLDER="true">BOOK</H3>\n    <DL><p>\n    </DL><p>\n'
         htmlFile.writelines(st)
     #获取url
-    with open('urlsList.txt', 'r') as scanFile:
-        urlsList = list(set([url.strip() for url in scanFile if url.strip()]))
+   try:
+        with open('urlsList.txt', 'r') as scanFile:
+            urlsList = list(set([url.strip() for url in scanFile if url.strip()]))
+    except:
+        pass
     #获取可访问页面
     print(" | 主线任务进度 |")
     with concurrent.futures.ThreadPoolExecutor() as executor:
