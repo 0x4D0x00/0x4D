6b3558758446d67d4d81642536da732e92e1f01c
0x4D0x00 <94094679+0x4D0x00@users.noreply.github.com>
2024-06-05 14:58:04 +0800

    Update scanPorts.py

--- scanPorts.py
+++ scanPorts.py
@@ -21,20 +21,18 @@ def scanPorts(ip):
         s.settimeout(0.1)
         try:
             r = s.connect_ex((ip, port))
-            if r == 0:#判断是否开启
+            if r == 0:# 判断是否开启
                 openportsQueue.put(f"{ip}:{port}")# 将结果放入队列
         except:
             pass
         finally:
             s.close()
             
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        # 批量提交任务给线程池
+    with concurrent.futures.ThreadPoolExecutor() as executor:# 批量提交任务给线程池
         futures = [executor.submit(sanner, port) for port in portList]
         concurrent.futures.wait(futures)# 等待所有任务完成
         
-    # 从队列中获取所有结果
-    openportsList = [openportsQueue.get() for _ in range(openportsQueue.qsize())]
+    openportsList = [openportsQueue.get() for _ in range(openportsQueue.qsize())]# 从队列中获取所有结果
     return openportsList
     
 def writeReport(ipsPorts):
@@ -67,8 +65,7 @@ def writeReport(ipsPorts):
     ip = str(ip)
     port = str(port)
     if port in portsFiles:
-        fileName = portsFiles[port]
-        #暂时不使用fileName
+        fileName = portsFiles[port]# 暂时不使用fileName
         highriskPortsList.append(f"{ip}:{port}")
     else:
         if ipsPorts not in urlsList:
@@ -78,14 +75,12 @@ def writeReport(ipsPorts):
 def clearingGarbages(openportsList, threshold):
     ipCounts = {}
     for ipPort in openportsList:
-        ip, _ = ipPort.split(":")  # 只获取IP部分
+        ip, _ = ipPort.split(":")# 只获取IP部分
         ipCounts[ip] = ipCounts.get(ip, 0) + 1
 
-    # 过滤掉IP出现次数大于阈值的对象
-    newopenportsList = list(set([ipPort for ipPort in openportsList if ipCounts[ipPort.split(":")[0]] <= threshold]))
+    newopenportsList = list(set([ipPort for ipPort in openportsList if ipCounts[ipPort.split(":")[0]] <= threshold]))# 过滤掉IP出现次数大于阈值的对象
 
-    # 记录垃圾内容
-    garbagesList = list(set([ipPort for ipPort in openportsList if ipCounts[ipPort.split(":")[0]] >= threshold]))
+    garbagesList = list(set([ipPort for ipPort in openportsList if ipCounts[ipPort.split(":")[0]] >= threshold]))# 记录垃圾内容
 
     for ipPort in garbagesList:
         ip, _ = ipPort.split(":")
@@ -99,8 +94,7 @@ def clearingGarbages(openportsList, threshold):
 
 if __name__ == "__main__":
     
-    # 读取文件
-    try:
+    try:# 读取文件
         with open('onlineIPs.txt', 'r') as file:
             ripsList = list(set(line.strip() for line in file if line.strip()))
         with open('onlineDomains.txt', 'r') as file:
@@ -109,36 +103,33 @@ if __name__ == "__main__":
             rdomainsbundledIpsList = list(set(line.strip() for line in file if line.strip()))
     except:
         pass
-    # 刷新
-    with open('highriskPorts.txt', 'w') as file:
+
+    with open('highriskPorts.txt', 'w') as file:# 刷新
         pass
-    temporaryList, urlsList, highriskPortsList = [], [], []
-    # 创建时间
-    timesTamp = str(int(time.time()))
+    
+    temporaryList, urlsList, highriskPortsList = [], [], []# 创建列表
+
+    timesTamp = str(int(time.time()))# 创建时间
     print(timesTamp)
     
-    print(" | 第 1 任务进度 |")
-    # 创建线程池
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        # 批量提交任务给线程池
+    print(" | 主线任务进度 |")# 创建线程池
+    with concurrent.futures.ThreadPoolExecutor() as executor:# 批量提交任务给线程池
         resultList = list(tqdm(executor.map(scanPorts, ripsList), total = len(ripsList)))
-    # 合并所有IP的开放端口列表（如果需要）
-    openportsList = [ipPort for ipsPorts in resultList for ipPort in ipsPorts]
+    openportsList = [ipPort for ipsPorts in resultList for ipPort in ipsPorts]# 合并所有IP的开放端口列表（如果需要）
     
     ###############################################################
     # 清除垃圾（阈值设置100：超过100个端口的ip将被记录在垃圾数据中）
     newopenportsList = clearingGarbages(openportsList, 100)
     ###############################################################
     
-    print(" | 第 2 任务进度 |")
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        # 批量提交任务给线程池
+    print(" | 支线任务进度 |")
+    with concurrent.futures.ThreadPoolExecutor() as executor:# 批量提交任务给线程池
         for result in tqdm(executor.map(writeReport, newopenportsList), total = len(newopenportsList)):
             pass
-    # 创建字典，存储映射    
-    ipDomainMap = {ip: domain for domain, ip in (entry.split(':') for entry in rdomainsbundledIpsList)}
-    # 过滤temporaryList中的IP，找出它们对应的域名，并从rdomainsList中删除这些域名
-    for ip in temporaryList:
+      
+    ipDomainMap = {ip: domain for domain, ip in (entry.split(':') for entry in rdomainsbundledIpsList)}# 创建字典，存储映射  
+
+    for ip in temporaryList:# 过滤temporaryList中的IP，找出它们对应的域名，并从rdomainsList中删除这些域名
         if ip in ipDomainMap:
             domain = ipDomainMap[ip]
             if domain in rdomainsList:
@@ -147,8 +138,8 @@ if __name__ == "__main__":
         if domain not in urlsList:
             urlsList.append(f"http://{domain}")
             urlsList.append(f"https://{domain}")
-    # 输出内容
-    with open('urlsList.txt', 'w') as file:
+    
+    with open('urlsList.txt', 'w') as file:# 输出内容
         file.writelines("\n".join(urlsList))
     with open('highriskPorts.txt', 'w') as file:
         file.write("\n".join(highriskPortsList))
