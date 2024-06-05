d980c2ca3b1ea83c652ca2abc26f6447248db792
0x4D0x00 <94094679+0x4D0x00@users.noreply.github.com>
2024-06-05 11:19:54 +0800

    Update pingipsDomains.py

--- pingipsDomains.py
+++ pingipsDomains.py
@@ -15,7 +15,7 @@ def pingipDomain(ipDomain):
         if 'ms' in str(response):
             if 'cname' in str(response):
                 if f"{ipDomain}" not in cnameipsdomainsList:
-                    cnameipsdomainsList.append(f"{ipDomain}")
+                    cnameipsdomainsList.append(f"{ipDomain}")                
             else:
                 ipAddress = re.search(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b', str(response))
                 ipAddress = ipAddress.group()
@@ -46,24 +46,37 @@ if __name__ == "__main__":
     timesTamp = str(int(time.time()))
     print(timesTamp)
     print(" | 主线任务进度 |")
-    cnameipsdomainsList, onlineipsdomainsList, offlineipsdomainsList, ipsaddressList, domainsList, domainsbundledIpsList = [], [], [], [], [], []
+    cnameipsdomainsList, onlineipsdomainsList, offlineipsdomainsList, ipsaddressList, domainsList, domainsbundledIpsList, domainsList = [], [], [], [], [], [], []
     # 创建线程池
     with concurrent.futures.ThreadPoolExecutor() as executor:
         # 批量提交任务给线程池
         for result in tqdm(executor.map(pingipDomain, ipsdomainsList), total=len(ipsdomainsList)):
-            pass
-        #写入报告
-        with open('domainsbundledIps.txt', 'w') as file:
-            file.write('\n'.join(domainsbundledIpsList))
-        with open('onlineIPs.txt', 'w') as file:
-            file.writelines('\n'.join(ipsaddressList))
-        with open('onlineDomains.txt', 'w') as file:
-            file.writelines('\n'.join(domainsList))
+            tqdm.update()
+        print(" | 支线任务进度 |")
+        # 尝试 bypass cname 域名
+        for domain in cnameipsdomainsList:
+            retrieval = re.search(r'\.[a-zA-Z0-9]{2,}\.[a-zA-Z]{2,}', domain)
+            domain = retrieval.group()[1:]
+            if f"{domain}" not in domainsList:
+                domainsList = append.(f"{domain}")
+        # 重置列表
+        cnameipsdomainsList = []
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+        # 批量提交任务给线程池
+            for result in tqdm(executor.map(pingipDomain, domainsList), total=len(domainsList)):
+                tqdm.update()
+            #写入报告
+            with open('domainsbundledIps.txt', 'w') as file:
+                file.write('\n'.join(domainsbundledIpsList))
+            with open('onlineIPs.txt', 'w') as file:
+                file.writelines('\n'.join(ipsaddressList))
+            with open('onlineDomains.txt', 'w') as file:
+                file.writelines('\n'.join(domainsList))
+            with open('cnameServers.txt', 'w') as file:
+                file.writelines('\n'.join(cnameipsdomainsList))
         '''以下内容备用
         #with open('onlineipsDomains.txt', 'w') as file:
             #file.writelines(f"{ipDomain}\n" for ipDomain in onlineipsdomainsList)
-        #with open('cnameServers.txt', 'w') as file:
-            #file.writelines(f"{ipDomain}\n" for ipDomain in cnameipsdomainsList)
         #with open('offlineServers.txt', 'w') as file:
             #file.writelines(f"{ipDomain}\n" for ipDomain in offlineipsdomainsList)
         '''
