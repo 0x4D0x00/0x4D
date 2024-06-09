ead504cc0311657c6ee009b2fea26026d30ecb05
0x4D0x00 <94094679+0x4D0x00@users.noreply.github.com>
2024-06-09 12:23:57 +0800

    Update pingipsDomains.py

--- pingipsDomains.py
+++ pingipsDomains.py
@@ -2,153 +2,142 @@
 ping ip 或 域名, 返回在线ip
 '''
 import re
-import time
+import platform
 import subprocess
 import dns.resolver
-import concurrent.futures
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
 
-def searchgithubdnsServers(ipDomain):
-    
+def get_ip(infomation_str):
     try:
-        resolver = dns.resolver.Resolver()
-        answers = resolver.resolve(ipDomain, 'A')
-        for ipAddress in answers:
-            dnsserversList.append(f"{ipAddress.address}")
-            if ipAddress is not None:
-                cnameipsdomainsList.remove(ipDomain)
-                domainsbundledIpsList.append(f"{ipDomain}:{ipAddress.address}")
-                ipsaddressList.append(f"{ipAddress.address}")
-                onlineipsdomainsList.append(f"{ipDomain}")
-                hostsList.append(f"{ipAddress.address} {ipDomain}")
-        return dnsserversList
+        ip_address = re.search(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b', str(infomation_str))   # 提取ip地址
+        ip_address = ip_address.group()
+        return str(ip_address)
     except Exception as e:
-        pass
-def pingipDomain(ipDomain):
-    
+        #print(e)
+        return None
+def ping_ip_domain(ip_domain):
+    # 发送ping命令
     try:
-        response = subprocess.run(['ping', '-n', '2', ipDomain], stdout = subprocess.PIPE, text = True, shell = True)   # ping并返回结果,如果你是Linux用户,请将-n改为-c
-        if 'ms' in str(response):   # 寻找存活目标
-            if 'cname' in str(response):    # 筛查附带云防御目标
-                if f"{ipDomain}" not in cnameipsdomainsList:
-                    cnameipsdomainsList.append(f"{ipDomain}")
-            else:
-                ipAddress = re.search(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b', str(response))   # 提取ip地址
-                ipAddress = ipAddress.group()
-                if f"{ipAddress}" not in ipsaddressList:
-                    ipsaddressList.append(f"{ipAddress}")
-                if f"{ipDomain}" not in onlineipsdomainsList:
-                    onlineipsdomainsList.append(f"{ipDomain}")
-                if ipAddress != ipDomain and f"{ipDomain}" not in domainsList:
-                    domainsList.append(f"{ipDomain}")
-                    if f"{ipDomain}:{ipAddress}" not in domainsbundledIpsList:
-                        domainsbundledIpsList.append(f"{ipDomain}:{ipAddress}")
+        Linux_args = ['ping', '-c', '1', ip_domain]
+        Windows_args = ['ping', '-n', '1', ip_domain]
+        result = subprocess.run(Linux_args if platform.system() == 'Linux' else Windows_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=1)
+        if 'ms' in str(result):
+            return True, str(result)
         else:
-            if f"{ipDomain}" not in offlineipsdomainsList:
-                offlineipsdomainsList.append(f"{ipDomain}")
-    except:
-        pass
-def cnameBypass(domain):
-
-    def domainCreat(domain):
-        for domainName in domainnamesList:
-            newdomain = f"{domainName}" + "." + f"{domain}"
-            if f"{newdomain}" not in newDomainsList:
-                newDomainsList.append(f"{newdomain}")
-                bypassList.append(f"{newdomain}:{domain}")
-        return newDomainsList, bypassList
-    try:
-        ipAddress = re.search(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b', str(domain))
-        if ipAddress:
-            newdomain = ipAddress.group()
-            if newdomain:
-                pass
-        else:
-            retrievaldomainName = re.search(r'\.[^.]+\.[^.]+$', str(domain))
-            if retrievaldomainName:
-                newdomain = retrievaldomainName.group()[1:]
-                if newdomain:
-                    if f"{newdomain}" not in newDomainsList:
-                        newDomainsList.append(f"{newdomain}")
-                        bypassList.append(f"{newdomain}:{domain}")
-            else:
-                domainCreat(domain)
+            if ip_domain not in off_line_List:
+                off_line_List.append(ip_domain)
+            return False
     except Exception as e:
-        print(f"{e}")
-    
-if __name__ == "__main__":
-    
-    fileName = 'ipsDomains.txt' # 读取文件
+        #print(e)
+        return False
+def retrieval_ips_domains(ip_domain):
+    result = ping_ip_domain(ip_domain)
+    if result:
+        ip_address = get_ip(str(result))
+        if 'cname' in str(result):
+            resolver = dns.resolver.Resolver()
+            answers = resolver.resolve(ip_domain, 'A')
+            for rdata in answers:
+                try:
+                    new_ip = get_ip(str(rdata.address))
+                except:
+                    new_ip = None
+                if new_ip is not None:
+                    x = re.search(r'[0-9]{1,3}\.', str(ip_address))
+                    x = x.group()[:-1]
+                    y = re.search(r'[0-9]{1,3}\.', str(new_ip))
+                    y = y.group()[:-1]
+                    if int(x) != int(y):
+                        bypass_hosts_List.append(f"{new_ip} {ip_domain}")
+                    else:
+                        cname_hosts_List.append(f"{ip_address} {ip_domain}")
+        else: 
+            if ip_address:
+                hosts_List.append(f"{ip_address} {ip_domain}")
+    return cname_hosts_List, bypass_hosts_List, hosts_List
+def cname_bypass(domain):
+    def bypass_retrieval(domain, new_domain):
+        try:
+            old_result = ping_ip_domain(str(domain))
+            new_result = ping_ip_domain(str(new_domain))
+        except Exception as e:
+            print(e)
+            new_result = 'cname'
+        if 'ms' in str(new_result) and 'cname' not in str(new_result):
+            cname_ip = get_ip(str(old_result))
+            new_ip = get_ip(str(new_result))
+            if new_ip is not None:
+                x = re.search(r'[0-9]{1,3}\.', str(cname_ip))
+                x = x.group()[:-1]
+                y = re.search(r'[0-9]{1,3}\.', str(new_ip))
+                y = y.group()[:-1]
+                if int(x) != int(y):
+                    if f"{new_ip} {domain}" not in bypass_hosts_List:
+                        bypass_hosts_List.append(f"{new_ip} {domain}")
+        return bypass_hosts_List
+    def domain_creat(domain):
+        for domain_name in domain_name_List:
+            new_domain = f"{domain_name}" + "." + f"{domain}"
+            bypass_retrieval(domain, new_domain)
     try:
-        with open(fileName, 'r') as file:
-            ipsdomainsList = list(set(line.strip() for line in file if line.strip()))
-    except:
-        with open(fileName, 'w') as file:
+        retrieval_domain_Name = re.search(r'\.[^.]+\.[^.]+$', str(domain))
+        if retrieval_domain_Name:
+            new_domain = retrieval_domain_Name.group()[1:]
+            bypass_retrieval(str(domain), str(new_domain))
+        else:
+            domain_creat(str(domain))
+    except Exception as e:
+        domain_creat(str(domain))
+        print(e)
+def multithreaded_processor(func, iterable):
+    
+    with ThreadPoolExecutor() as executor:
+        for result in tqdm(executor.map(func, iterable), total=len(iterable)):
             pass
+
+if __name__ == '__main__':
+    try:
+        with open('ipsDomains.txt', 'r') as file:
+            ips_domains_List = list(set(line.strip() for line in file if line.strip()))
+    except Exception as e:
+        print(e)
+        with open('ipsDomains.txt', 'w') as file:
+            pass
+        ips_domains_List = []
         print('ipsDomains.txt文件内容为空。')
-    timesTamp = str(int(time.time()))   # 创建时间
-    print(timesTamp)
-    print(" | First job |")
-    cnameipsdomainsList, onlineipsdomainsList, offlineipsdomainsList, ipsaddressList, domainsList, domainsbundledIpsList, newDomainsList, bypassList, hostsList, dnsserversList = [], [], [], [], [], [], [], [], [], []    # 创建列表
-    with concurrent.futures.ThreadPoolExecutor() as executor:   # 第1次ping
-        for result in tqdm(executor.map(pingipDomain, ipsdomainsList), total=len(ipsdomainsList)):  # 批量提交任务给线程池
-            pass
-    print(" | Serach cname history dns record |")
-    with concurrent.futures.ThreadPoolExecutor() as executor:   # 查看cname域名历史dns记录
-        for result in tqdm(executor.map(searchgithubdnsServers, cnameipsdomainsList), total=len(cnameipsdomainsList)): # 批量提交任务给线程池
-            pass
-    print(" | Second job |")
-    with concurrent.futures.ThreadPoolExecutor() as executor:   # 第2次ping
-        for result in tqdm(executor.map(pingipDomain, dnsserversList), total=len(dnsserversList)): # 批量提交任务给线程池
-            pass
+    on_line_List = []
+    on_line_ip_List = []
+    on_line_domain_List = []
+    off_line_List = []
+    cname_hosts_List = []
+    bypass_hosts_List = []
+    hosts_List = []
+    cname_domain_List = []
+    bypass_domain_List = []
+    print("ping ips and domains")
+    multithreaded_processor(retrieval_ips_domains, ips_domains_List)
     try:
-        with open("domainnamesDict.txt", 'r') as file:  # 读取域名字典
-            domainnamesList = list(set(line.strip() for line in file if line.strip()))
-    except:
-        domainnamesList = []
-        print('domainnamesDict.txt文件内容为空。')
-    print(" | Cname bypass |")
-    with concurrent.futures.ThreadPoolExecutor() as executor:   # cname域名bypass尝试
-        for result in tqdm(executor.map(cnameBypass, cnameipsdomainsList), total=len(cnameipsdomainsList)): # 批量提交任务给线程池
-            pass
-    with open('oldcnameServers.txt', 'w') as file:
-        file.writelines('\n'.join(cnameipsdomainsList))
-    with open('domainsbundledIps.txt', 'w') as file:    #写入报告
-        file.write('\n'.join(domainsbundledIpsList))
-    cnameipsdomainsList = []    # 重置记录
-    domainsbundledIpsList = []
-    print(" | Last job |")
-    with concurrent.futures.ThreadPoolExecutor() as executor:   # 第3次ping
-        for result in tqdm(executor.map(pingipDomain, newDomainsList), total=len(newDomainsList)):  # 批量提交任务给线程池
-            pass
-    for bundledItem in domainsbundledIpsList:
-        domain, ip = bundledItem.split(':')
-        for bypassItem in bypassList:
-            bypassDomain, targetDomain = bypassItem.split(':')
-            if domain == bypassDomain: # 如果找到相同的第一个值，则组合为'ip targetDomain'并添加到hostsList中
-                if f"{ip} {targetDomain}" not in hostsList:
-                    hostsList.append(f'{ip} {targetDomain}')
-    #class后转为调用其他函数
-    with open('domainsbundledIps.txt', 'a') as file:    #写入报告
-        file.write('\n'.join(domainsbundledIpsList))
-    with open('onlineIPs.txt', 'w') as file:
-        file.write('\n'.join(ipsaddressList))
-    with open('onlineDomains.txt', 'w') as file:
-        file.write('\n'.join(domainsList))
-    with open('newcnameServers.txt', 'w') as file:
-        file.write('\n'.join(cnameipsdomainsList))
-    try:
-        with open('C:\\Windows\\System32\\drivers\\etc\\hosts', 'a') as file:   # 改写hosts文件，使用真实ip与域名绑定
-            file.write('\n'.join(hostsList))
-    except:
-        with open('hosts', 'w') as file:
-            file.write('\n'.join(hostsList))
-    print(f"在线数量:{len(onlineipsdomainsList)}")
-    print(f"离线数量:{len(offlineipsdomainsList)}")
-    '''以下内容备用
-    with open('onlineipsDomains.txt', 'w') as file:
-        file.writelines(f"{ipDomain}\n" for ipDomain in onlineipsdomainsList)
-    with open('offlineServers.txt', 'w') as file:
-        file.writelines(f"{ipDomain}\n" for ipDomain in offlineipsdomainsList)
-    '''
+        with open("domainnamesDict2.txt", 'r') as file:  # 读取域名字典
+            domain_name_List = list(set(line.strip() for line in file if line.strip()))
+    except Exception as e:
+        print(e)
+        domain_name_List = []
+    print("bypass cname domains")
+    for ip_domain in cname_hosts_List:
+        ip, domain = ip_domain.split(' ')
+        cname_domain_List.append(domain)
+    multithreaded_processor(cname_bypass, cname_domain_List)
+    if bypass_hosts_List is not None:
+        with open('C:\\Windows\\System32\\drivers\\etc\\hosts', 'w') as file:   # 改写hosts文件，使用真实ip与域名绑定
+            file.write('\n'.join(bypass_hosts_List))
+    for ips_domainS in bypass_hosts_List:
+        if ips_domainS not in hosts_List:
+            hosts_List.append(ips_domainS)
+    with open('hosts.txt', 'w') as file:
+        file.write('\n'.join(hosts_List))
+    print(f"on line ips:{len(hosts_List)}")
+    print(f"off line ips:{len(off_line_List)}")
+    print(f"cname domains:{len(cname_hosts_List)}")
+    print(f"bypass cname数量:{len(bypass_hosts_List)}")
     print("任务执行完成")
