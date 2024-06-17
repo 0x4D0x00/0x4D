5ea5900fe88fdd239c5acf11c9639dbd040ac4d1
0x4D <94094679+0x4D0x00@users.noreply.github.com>
2024-06-17 10:10:54 +0800

    Update multiprocessService.py

--- multiprocessService.py
+++ multiprocessService.py
@@ -1,30 +1,37 @@
-'''
-Created on 2024. 6. 1
-@author: 0x4D
-multi process service
-'''
-class multiProcess:
-    def __init__(self, function, iterable):
-        self.function = function
-        self.iterable = list(iterable)
-        self.data = self.distribute_tasks()
-    def distribute_tasks(self):
-        result_List = []
-        import concurrent.futures
-        from tqdm import tqdm
-        function = self.function
-        iterable = self.iterable
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            # 多线程分发任务func = 需要调用的函数名，iterable = func该函数需要用到的列表
-        #with concurrent.futures.ThreadPoolExecutor(max_workers=x) as executor:   # 设置最大线程数为x, x为整数，范围为1-100，如果有资源限制，请设置为大于0小于等于100的整数。
-            for result in tqdm(executor.map(function, iterable), total=len(iterable)):
-                result_List.append(result)
-            return result_List
 
+'''
+多进程服务模块
+用于并行执行多个任务，提高处理效率。
+'''
+
+from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
+
+class MultiProcessService:
+    def __init__(self, function, iterable, max_workers=10):
+        """初始化多进程服务。
+        :param function: 要并行执行的函数。
+        :param iterable: 需要处理的数据集。
+        :param max_workers: 线程池中的最大工作线程数。
+        """
+        self.function = function
+        self.iterable = iterable
+        self.max_workers = max_workers
+
+    def execute(self):
+        """执行并行处理任务。
+        :return: 执行结果列表。
+        """
+        with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+            results = list(tqdm(executor.map(self.function, self.iterable), total=len(self.iterable)))
+        return results
+
+# 示例用法
 if __name__ == '__main__':
-    def get_ip(information_str):
-        from getService import get
-        return get(information_str).data
-    my_list = ['asd 111.111.111.111 asd', ' asd 222.222.222.222 asd ', ' gasf333.333.333.333', 'fqwe 444.444.444.444'
-                , '555.555.555.555 asd', '666.666.666.666gfqe', '777.777.777.777gdsa', 'asdf888.888.888.888']
-    print(multiProcess(get_ip, my_list).data)
+    def square(x):
+        return x * x
+
+    numbers = range(10)
+    mp_service = MultiProcessService(square, numbers)
+    results = mp_service.execute()
+    print('Squared numbers:', results)
