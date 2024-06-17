b50b82eb7f084ef0acbed5ef5858fcdb0fcc9a97
0x4D <94094679+0x4D0x00@users.noreply.github.com>
2024-06-17 16:10:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,12 @@
 主操作模块
 用于处理网络操作，包括发送 ping 请求和从响应中提取 IP 地址。
 '''
-
+import re
 from getServiceOptimized import IPExtractor
 from pingServiceOptimized import PingService
 from readwriteServiceOptimized import ReadWriteService
 from NslookupService import NslookupService
+from multiprocessServiceOptimized import MultiProcessService
 
 class NetworkOperations:
     def __init__(self, domain_list_file):
@@ -28,11 +29,11 @@ class NetworkOperations:
         """
         return self.ping_service.ping(ip_domain)[1]
     def nslookup_ip_domain(self, ip_domain):
-        """发送 ping 命令到指定的 IP 或域名。
-        :param ip_domain: 要 ping 的 IP 或域名。
-        :return: ping 命令的响应数据。
+        """发送 nslookup 命令到指定的 IP 或域名。
+        :param ip_domain: 要 nslookup 的 IP 或域名。
+        :return: nslookup 命令的响应数据。
         """
-        return self.nslookup_service.nslookup(ip_domain)[1]
+        return self.nslookup_service.nslookup(ip_domain)
 
     def get_ip(self, information_str):
         """从信息字符串中提取 IP 地址。
@@ -57,28 +58,25 @@ class NetworkOperations:
         """
         result_ping = self.ping_ip_domain(ip_domain)
         result_nslookup = self.nslookup_ip_domain(ip_domain)
-        if result_ping and result_nslookup:
-            ip_address = self.get_ip(result_ping)
-            domain_address_nslookup = self.get_domain(result_nslookup)
-            if ip_address is not None and domain_address_nslookup:
-                if ip_domain != domain_address_nslookup:
+        ip_address = self.get_ip(result_ping)
+        if ip_address is not None:
+            nslookup_data = f"{result_nslookup}"
+            if "名称:" in nslookup_data:
+                real_domain = re.findall(r'名称:\s+(\S+)', nslookup_data)[0]
+                if ip_domain != real_domain and f"{ip_address}" in nslookup_data:
                     if f"{ip_address} {ip_domain}" not in self.cname_hosts_list:
                         self.cname_hosts_list.append(f"{ip_address} {ip_domain}")
                 else:
-                    if ip_address:
-                        self.hosts_list.append(f"{ip_address} {ip_domain}")
+                    self.hosts_list.append(f"{ip_address} {ip_domain}")
         return self.cname_hosts_list, self.hosts_list
-
     def perform_network_operations(self):
         """执行网络操作，包括 ping 域名并提取 IP 地址，处理 CNAME 和非 CNAME 的情况。
         """
-        for domain in self.domain_list:
-            cname_list, host_list = self.retrieval_ips_domains(domain)
-            if cname_list:
-                print(f'CNAME hosts for domain {domain}: {cname_list}')
-            if host_list:
-                print(f'Hosts for domain {domain}: {host_list}')
-
+        domain_list = self.domain_list
+        MultiProcessService(self.retrieval_ips_domains, domain_list).data
+        print(self.cname_hosts_list)
+        print(self.hosts_list)
+        
 # 示例用法
 if __name__ == '__main__':
     ops = NetworkOperations('domain_list.txt')
