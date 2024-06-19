0d7462bfd8e62ce6e27e1a2f8a02f2a4fff9323b
0x4D <94094679+0x4D0x00@users.noreply.github.com>
2024-06-19 11:38:18 +0800

    Update bookMarkOptimized.py

--- bookMark.py
+++ bookMark.py
@@ -1,65 +1,65 @@
 '''
-访问url链接, 返回可以访问的url并做成书签，方便burpsuite一键开启，以及返回一张方便nmap和goby等扫描工具扫描的txt文件
+访问url链接, 返回可以访问的url并做成书签,方便burpsuite一键开启,以及返回一张方便nmap和goby等扫描工具扫描的txt文件
 '''
 import re
 import time
 import requests
-import concurrent.futures
-from tqdm import tqdm
+from multiprocessServiceOptimized import MultiProcessService
+from readwriteServiceOptimized import ReadWriteService
 
-def scanner(url):
+class BookMarkService:
+    def __init__(self, data_path="urlsList.txt", write_method='a', book_mark_path='bookMark.html', nmap_scan_payh='nmapScan.txt'):
+        self.data_path = data_path
+        self.book_mark_path = book_mark_path
+        self.nmap_scan_payh = nmap_scan_payh
+        self.write_method = write_method
+        self.iis7_list = []
+        self.nginx_list = []
+        self.other_list = []
+        self.new_ips_domains_list = []
+        self.body_list = []
     
-    try:
-        response = requests.get(url, timeout = 5)
-        content = response.text
-        statusCode = response.status_code
-        if statusCode == 200:
-            if "IIS7" in content:
-                if f"{url}" not in iis7List:
-                    iis7List.append(f"{url}")
-            elif "Welcome to nginx!" in content:
-                if f"{url}" not in nginxList:
-                    nginxList.append(f"{url}")
-            elif "限制" not in content and "无法正常工作" not in content and "认证失败" not in content and "window.wx" not in content and "请稍后再试" not in content and "防火墙" not in content and "云防御" not in content:
-                if f"{url}" not in otherList:
-                    otherList.append(f"{url}")
-                    pattern = r"(https?://)|(:\d{1,5})"
-                    newipDomain = re.sub(pattern, "", url)
-                    if f"{newipDomain}" not in newipsdomainsList:
-                        newipsdomainsList.append(f"{newipDomain}")
-    except:
-        pass
+    def scanner(self, url):
+    
+        try:
+            response = requests.get(url, timeout = 5)
+            content = response.text
+            statusCode = response.status_code
+            if statusCode == 200:
+                if "IIS7" in content:
+                    if f"{url}" not in self.iis7_list:
+                        self.iis7_list.append(f"{url}")
+                elif "Welcome to nginx!" in content:
+                    if f"{url}" not in self.nginx_list:
+                        self.nginx_list.append(f"{url}")
+                elif "限制" not in content and "无法正常工作" not in content and "认证失败" not in content and "window.wx" not in content and "请稍后再试" not in content and "防火墙" not in content and "云防御" not in content and "errcode" not in content and "Not Found" not in content:
+                    if f"{url}" not in self.other_list:
+                        self.other_list.append(f"{url}")
+                        pattern = r"(https?://)|(:\d{1,5})"
+                        newipDomain = re.sub(pattern, "", url)
+                        if f"{newipDomain}" not in self.new_ips_domains_list:
+                            self.new_ips_domains_list.append(f"{newipDomain}")
+        except:
+            pass
+
+    def write_book_mark(self):
+        def write_body(url):
+            body = f'	<DT><A HREF="{url}" ADD_DATE={Datetime}">{url}</A>\n'
+            if body not in self.body_list:
+                self.body_list.append(body)
+        Datetime = str(int(time.time()))
+        head = [f'<!DOCTYPE NETSCAPE-Bookmark-file-1>\n<!-- This is an automatically generated file.\n     It will be read and overwritten.\n     DO NOT EDIT! -->\n<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">\n<TITLE>Bookmarks</TITLE>\n<H1>Bookmarks</H1>\n<DL><p>\n    <DT><H3 ADD_DATE="2716597092" LAST_MODIFIED="0" PERSONAL_TOOLBAR_FOLDER="true">BOOK</H3>\n    <DL><p>\n    </DL><p>\n']
+        ReadWriteService(self.book_mark_path).write_txt(head)
+        urlsList = ReadWriteService(self.data_path).read_txt()
+        print(" | 主线任务进度 |")
+        MultiProcessService(self.scanner, urlsList).execute()
+        MultiProcessService(write_body, self.other_list).execute()
+        ReadWriteService(self.book_mark_path, self.write_method).write_txt(self.body_list)
+        dlp = ['</DL><p>\n']
+        ReadWriteService(self.book_mark_path, self.write_method).write_txt(dlp)
+        ReadWriteService(self.nmap_scan_payh).write_txt(self.new_ips_domains_list)
+        print(" | 主线任务完成 |")
 
 if __name__ == "__main__":
-    
-    iis7List, nginxList, otherList, newipsdomainsList = [], [], [], []# 刷新
-    Datetime = str(int(time.time()))
+    BookMarkService().write_book_mark()
 
-    with open('bookMark.html', 'w') as file:# 创建书签
-        st = f'<!DOCTYPE NETSCAPE-Bookmark-file-1>\n<!-- This is an automatically generated file.\n     It will be read and overwritten.\n     DO NOT EDIT! -->\n<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">\n<TITLE>Bookmarks</TITLE>\n<H1>Bookmarks</H1>\n<DL><p>\n    <DT><H3 ADD_DATE="2716597092" LAST_MODIFIED="0" PERSONAL_TOOLBAR_FOLDER="true">BOOK</H3>\n    <DL><p>\n    </DL><p>\n'
-        file.writelines(st)
-    
-    try:# 获取url
-        with open('urlsList.txt', 'r') as file:
-            urlsList = list(set([url.strip() for url in file if url.strip()]))
-    except:
-        pass
-    
-    print(" | 主线任务进度 |")
-    with concurrent.futures.ThreadPoolExecutor() as executor:# 获取可访问页面
-        for result in tqdm(executor.map(scanner, urlsList), total=len(urlsList)):
-            pass
-        
-    with open('bookMark.html', 'a') as file:
-        file.writelines(f'	<DT><A HREF="{url}" ADD_DATE={Datetime}">{url}</A>\n' for url in otherList)
-    with open('bookMark.html', 'a') as file:# 书签写入完成
-        dlp = '</DL><p>\n'
-        file.write(dlp)
-    
-    with open('nginxPorts.txt', 'w') as file:
-        file.writelines("\n".join(nginxList))
-    with open('IIS7Ports.txt', 'w') as file:
-        file.writelines("\n".join(iis7List))
-    with open('nmapScan.txt', 'w') as file:
-        file.writelines("\n".join(newipsdomainsList))
-    print('任务执行完成')
