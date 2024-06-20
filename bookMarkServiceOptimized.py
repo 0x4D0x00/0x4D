1915cd18c7e1737a08c39c2bade9beb5ba96ae87
0x4D <94094679+0x4D0x00@users.noreply.github.com>
2024-06-20 19:33:27 +0800

    Update bookMarkServiceOptimized.py

--- bookMarkServiceOptimized.py
+++ bookMarkServiceOptimized.py
@@ -6,43 +6,23 @@ import time
 import requests
 from multiprocessServiceOptimized import MultiProcessService
 from readwriteServiceOptimized import ReadWriteService
+from accesscheckServiceOptimized import AccessCheckService
 
 class BookMarkService:
-    def __init__(self, data_path="urlsList.txt", write_method='a', book_mark_path='bookMark.html', nmap_scan_payh='nmapScan.txt'):
-        self.data_path = data_path
+    def __init__(self, write_method='a', book_mark_path='bookMark.html', nmap_scan_payh='nmapScan.txt'):
         self.book_mark_path = book_mark_path
         self.nmap_scan_payh = nmap_scan_payh
         self.write_method = write_method
-        self.iis7_list = []
-        self.nginx_list = []
-        self.other_list = []
         self.new_ips_domains_list = []
         self.body_list = []
-    
-    def scanner(self, url):
-    
-        try:
-            response = requests.get(url, timeout = 5)
-            content = response.text
-            statusCode = response.status_code
-            if statusCode == 200:
-                if "IIS7" in content:
-                    if f"{url}" not in self.iis7_list:
-                        self.iis7_list.append(f"{url}")
-                elif "Welcome to nginx!" in content:
-                    if f"{url}" not in self.nginx_list:
-                        self.nginx_list.append(f"{url}")
-                elif "限制" not in content and "无法正常工作" not in content and "认证失败" not in content and "window.wx" not in content and "请稍后再试" not in content and "防火墙" not in content and "云防御" not in content and "errcode" not in content and "Not Found" not in content:
-                    if f"{url}" not in self.other_list:
-                        self.other_list.append(f"{url}")
-                        pattern = r"(https?://)|(:\d{1,5})"
-                        newipDomain = re.sub(pattern, "", url)
-                        if f"{newipDomain}" not in self.new_ips_domains_list:
-                            self.new_ips_domains_list.append(f"{newipDomain}")
-        except:
-            pass
-
-    def write_book_mark(self):
+    def domain_check(target):
+        """发送 HTTP或HTTPS 请求。
+        :param target: 要请求的域名。
+        :return: 提取到的网站页面信息。
+        """
+        result = AccessCheckService(target)
+        return result.maker_urls()
+    def write_book_mark(self, data_list=ReadWriteService("bookMark.txt").read_txt()):
         def write_body(url):
             body = f'	<DT><A HREF="{url}" ADD_DATE={Datetime}">{url}</A>\n'
             if body not in self.body_list:
@@ -50,14 +30,12 @@ class BookMarkService:
         Datetime = str(int(time.time()))
         head = [f'<!DOCTYPE NETSCAPE-Bookmark-file-1>\n<!-- This is an automatically generated file.\n     It will be read and overwritten.\n     DO NOT EDIT! -->\n<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">\n<TITLE>Bookmarks</TITLE>\n<H1>Bookmarks</H1>\n<DL><p>\n    <DT><H3 ADD_DATE="2716597092" LAST_MODIFIED="0" PERSONAL_TOOLBAR_FOLDER="true">BOOK</H3>\n    <DL><p>\n    </DL><p>\n']
         ReadWriteService(self.book_mark_path).write_txt(head)
-        urlsList = ReadWriteService(self.data_path).read_txt()
         print(" | 主线任务进度 |")
-        MultiProcessService(self.scanner, urlsList).execute()
-        MultiProcessService(write_body, self.other_list).execute()
+        urls_list = MultiProcessService(self.domain_check, data_list).execute()
+        MultiProcessService(write_body, urls_list).execute()
         ReadWriteService(self.book_mark_path, self.write_method).write_txt(self.body_list)
         dlp = ['</DL><p>\n']
         ReadWriteService(self.book_mark_path, self.write_method).write_txt(dlp)
-        ReadWriteService(self.nmap_scan_payh).write_txt(self.new_ips_domains_list)
         print(" | 主线任务完成 |")
 
 if __name__ == "__main__":
