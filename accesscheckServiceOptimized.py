9cd527290ff06303fa56e079294b65f53d4fd2fa
0x4D <94094679+0x4D0x00@users.noreply.github.com>
2024-06-21 08:33:59 +0800

    Update accesscheckServiceOptimized.py

--- accesscheckServiceOptimized.py
+++ accesscheckServiceOptimized.py
@@ -4,7 +4,7 @@
 class AccessCheckService:
     def __init__(self, target):
         self.target = target
-        self.user_agent = "User-Agent: Mozilla/5.0 (hp-tablet; Linux; hpwOS/3.0.0; U; en-US) AppleWebKit/534.6 (KHTML, like Gecko) wOSBrowser/233.70 Safari/534.6 TouchPad/1.0"
+        self.user_agent = "Mozilla/5.0 (hp-tablet; Linux; hpwOS/3.0.0; U; en-US) AppleWebKit/534.6 (KHTML, like Gecko) wOSBrowser/233.70 Safari/534.6 TouchPad/1.0"
         self.cookies = ""
     def access_service(self):
         '''
@@ -19,7 +19,7 @@ class AccessCheckService:
             response = requests.get(self.target, headers=headers, timeout = 5)
             content = response.text
             self.cookies = response.cookies
-            if response.status_code == 200 and "Not Found" not in content and "errcode" not in content and "Forbidden" not in content and "Unauthorized" not in content and "Bad Request" not in content:
+            if response.status_code == 200 and "Not Found" not in content and "Forbidden" not in content and "Unauthorized" not in content and "Bad Request" not in content:
                 return f"{content}"
             else:
                 return None
@@ -30,10 +30,10 @@ class AccessCheckService:
         '''
         将域名生成url访问检测
         '''
-        self.target = f"http://{self.target}"
+        self.target = f"https://{self.target}"
         content  = self.access_service()
         if content is None:
-            self.target = f"https://{self.target}"
+            self.target = f"http://{self.target}"
             content  = self.access_service()
             if content is None:
                 return "None"
@@ -42,13 +42,13 @@ class AccessCheckService:
         else:
             return content
     def maker_urls(self):
-        self.target = f"http://{self.target}"
+        self.target = f"https://{self.target}"
         content  = self.access_service()
         if content is None:
-            self.target = f"https://{self.target}"
+            self.target = f"http://{self.target}"
             content  = self.access_service()
             if content is None:
-                pass
+                return ""
             else:
                 return f"{self.target}"
         else:
@@ -57,17 +57,17 @@ class AccessCheckService:
 # 示例用法
 if __name__ == "__main__":
     from multiprocessServiceOptimized import MultiProcessService
-    domain_list = ["www.baidu.com", "www.google.com", "www.bing.com"]
+    domain_list = ["www.realworldctf.com"]
     def domain_check(target):
         """发送 HTTP或HTTPS 请求。
         :param target: 要请求的域名。
         :return: 提取到的网站页面信息。
         """
         result = AccessCheckService(target)
-        return result.check_access()
+        return result.maker_urls()
     result = MultiProcessService(domain_check, domain_list).execute()
     print(result)
-    
+'''    
     url_list = ["http://www.baidu.com", "http://www.google.com", "http://www.bing.com"]
     def url_check(target):
         """发送 HTTP或HTTPS 请求。
@@ -78,8 +78,9 @@ if __name__ == "__main__":
         return result.access_service()
     result = MultiProcessService(url_check, url_list).execute()
     print(result)
+  
 
-    url_list = ["www.baidu.com", "www.google.com", "www.bing.com"]
+    domain_list = ["www.realworldctf.com"]
     def urls_maker(target):
         """发送 HTTP或HTTPS 请求。
         :param target: 要请求的URL链接。
@@ -87,5 +88,6 @@ if __name__ == "__main__":
         """
         result = AccessCheckService(target)
         return result.maker_urls()
-    result = MultiProcessService(urls_maker, url_list).execute()
+    result = MultiProcessService(urls_maker, domain_list).execute()
     print(result)
+'''
